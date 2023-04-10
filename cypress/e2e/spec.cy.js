5e912952d4c227f2cc682282725b2587947f9a10
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-04-10 12:09:35 -0600

    Add fixture and begin cypress testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,29 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+import allTricks from '../fixtures/allTricks'
+
+describe('user flow', () => {
+
+  beforeEach(() => {
+    cy.intercept({method:'GET', url: 'http://localhost:3001/api/v1/tricks'}, allTricks )
+    cy.visit('http://localhost:3000')
   })
+
+  it('user should view page title page load', () => {
+    cy.get('h1')
+      .should('exist')
+      .should('be.visible')
+      .contains('Sick Trick')
+  })
+
+  it('user view input fields to enter their own trick', () => {
+    cy.get('form')
+      .should('exist')
+      .should('be.visible')
+  })
+
+  it('user should view all tricks on page load', () => {
+    cy.get('div')
+      .should('exist')
+
+  })
+
 })
